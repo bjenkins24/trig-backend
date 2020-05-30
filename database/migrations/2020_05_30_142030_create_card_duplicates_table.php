7833ce687d2140c3230d6e77042db90305a9b91f
Brian Jenkins <bjenkins@yourmusiclessons.com>
2020-05-30 07:24:27 -0700

    Create migration for card_duplicates table

---
+++ database/migrations/2020_05_30_142030_create_card_duplicates_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCardDuplicatesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('card_duplicates', function (Blueprint $table) {
+            $table->id();
+            $table->foreign('primary_card_id')->references('id')->on('cards');
+            $table->foreign('duplicate_card_id')->references('id')->on('cards');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('card_duplicates');
+    }
+}
