4a441eb00cab6d1a568f46d7f01aa8fd2b053e10
Brian Jenkins <brian@trytrig.com>
2020-07-19 13:12:49 -0700

    More cleanup

--- app/Mail/ForgotPasswordMail.php
+++ app/Mail/ForgotPasswordMail.php
@@ -5,6 +5,7 @@ namespace App\Mail;
 use Illuminate\Bus\Queueable;
 use Illuminate\Mail\Mailable;
 use Illuminate\Queue\SerializesModels;
+use Illuminate\Support\Facades\Config;
 
 class ForgotPasswordMail extends Mailable
 {
@@ -13,25 +14,18 @@ class ForgotPasswordMail extends Mailable
 
     /**
      * The string URL that will be attached to the reset password email.
-     *
-     * @var string
      */
-    public $resetUrl;
+    public string $resetUrl;
 
     /**
      * Create a new message instance.
      */
     public function __construct(string $token, string $emailHash)
     {
-        $this->resetUrl = \Config::get('app.client_url').'/reset-password/'.$token.'/'.$emailHash;
+        $this->resetUrl = Config::get('app.client_url').'/reset-password/'.$token.'/'.$emailHash;
     }
 
-    /**
-     * Build the message.
-     *
-     * @return $this
-     */
-    public function build()
+    public function build(): ForgotPasswordMail
     {
         return $this
             ->subject('Trig: Forgot Password Link')
