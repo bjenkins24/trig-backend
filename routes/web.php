2ee6b8b85c59dd6406fea7f0ae3c4feb370baa27
Brian Jenkins <brian@trytrig.com>
2020-08-14 17:47:53 -0700

    Test sentry

--- routes/web.php
+++ routes/web.php
@@ -17,6 +17,10 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::get('/debug-sentry', function () {
+    throw new Exception('My first Sentry error!');
+});
+
 Route::get('/health', function () {
     echo 'SUCCESS';
 
