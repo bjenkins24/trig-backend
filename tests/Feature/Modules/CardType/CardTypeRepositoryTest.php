d09dcb02f225c21ab81b27b2ff6ae7709b48d529
Brian Jenkins <brian@trytrig.com>
2020-12-20 08:09:27 -0800

    Amplitude test as well

--- tests/Feature/Modules/CardType/CardTypeRepositoryTest.php
+++ tests/Feature/Modules/CardType/CardTypeRepositoryTest.php
@@ -30,6 +30,7 @@ class CardTypeRepositoryTest extends TestCase
             ['video/3gpp2', 'Video', ''],
             ['link', 'YouTube', 'https://www.youtube.com/watch?v=dQw4w9WgXcQ'],
             ['link', 'Google Doc', 'https://docs.google.com/document/d/2L39XrmRn5MSiokVXc4c5ipEkdhDYYZUdbHJltaasdViPT0/edit'],
+            ['link', 'Amplitude', 'https://amplitude.com/reallycoolchart'],
             ['link', 'Link', 'https://medium.com/@noah_weiss/50-articles-and-books-that-will-make-you-a-great-product-manager-aad5babee2f7'],
             ['noway', 'Unknown', ''],
         ];
