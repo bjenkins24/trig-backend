7a536ef201e227411473e792f58200b305dc51f2
Brian Jenkins <brian@trytrig.com>
2020-12-23 15:18:29 -0800

    Fix some copy in welcome email

--- resources/views/emails/welcome.blade.php
+++ resources/views/emails/welcome.blade.php
@@ -4,15 +4,15 @@
     <p>
         Welcome to Trig. We're so happy to have you here!
     </p>
-    
+
     <p style="text-align: center">
         <a href="https://app.trytrig.com" style="display: inline-block; font-weight: 500; padding: 16px 32px; margin: 32px auto; background: #009E8F; color: #F5F5F5; text-decoration: none; text-align: center; border-radius: 4px;">
-            Go to you account
+            Go to your account
         </a>
     </p>
 
     <p>
-        Don't worry we're here to help. Reply to this or any of our other emails, 
-        and we'll be more than happy to help on your journey to an organized team.
+        We're here to help. Reply to this or any of our other emails,
+        and we'll be more than happy to help on your journey to organization.
     </p>
 @endsection
