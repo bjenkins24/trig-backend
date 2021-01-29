03aec83d3d8ce1dbb21d1a677ff8e9077935d963
Brian Jenkins <brian@trytrig.com>
2021-01-29 15:48:42 -0800

    optimize images

--- app/Modules/Card/Helpers/ThumbnailHelper.php
+++ app/Modules/Card/Helpers/ThumbnailHelper.php
@@ -9,6 +9,14 @@ use Exception;
 use Illuminate\Support\Collection;
 use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Facades\Storage;
+use Intervention\Image\Facades\Image;
+use Spatie\ImageOptimizer\OptimizerChain;
+use Spatie\ImageOptimizer\Optimizers\Cwebp;
+use Spatie\ImageOptimizer\Optimizers\Gifsicle;
+use Spatie\ImageOptimizer\Optimizers\Jpegoptim;
+use Spatie\ImageOptimizer\Optimizers\Optipng;
+use Spatie\ImageOptimizer\Optimizers\Pngquant;
+use Spatie\ImageOptimizer\Optimizers\Svgo;
 
 class ThumbnailHelper
 {
@@ -43,6 +51,57 @@ class ThumbnailHelper
         ]);
     }
 
+    public function testsomething(string $thing)
+    {
+        $image = $this->getImage($thing);
+        $result = Storage::put('public/card-images/test/hello.png', file_get_contents($thing));
+    }
+
+    /**
+     * This will save the image with compression - we are preferring quality over smaller size
+     * please only compress images in a lossless format.
+     *
+     * @throws Exception
+     */
+    public function saveImage(?string $uri, string $finalPath, Collection $image = null): Collection
+    {
+        if (! $image) {
+            $image = $this->getImage($uri);
+        }
+        $tmpName = bin2hex(random_bytes(16));
+        $path = 'public/tmp/'.$tmpName.'.'.$image->get('extension');
+        $fullTmpPath = 'storage/app/public/tmp/'.$tmpName.'.'.$image->get('extension');
+        try {
+            Storage::disk('local')->put($path, $image->get('image')->encode($image->get('extension'))->__toString());
+            $optimizerChain = (new OptimizerChain())
+                ->setTimeout(5)
+                ->addOptimizer(new Jpegoptim([
+                    '--strip-all',
+                    '--all-progressive',
+                ]))
+                ->addOptimizer(new Pngquant())
+                ->addOptimizer(new Optipng())
+                ->addOptimizer(new Svgo())
+                ->addOptimizer(new Gifsicle())
+                ->addOptimizer(new Cwebp());
+
+            $optimizerChain->optimize($fullTmpPath);
+            $image = $this->getImage($fullTmpPath);
+        } catch (Exception $exception) {
+            Log::error('Optimizing the image '.$uri.' failed: '.$exception);
+        }
+        $result = Storage::put('public'.$finalPath.'.'.$image->get('extension'), file_get_contents($fullTmpPath));
+        Storage::disk('local')->delete($path);
+
+        return collect([
+            'successful' => $result,
+            'extension'  => $image->get('extension'),
+        ]);
+    }
+
+    /**
+     * @throws Exception
+     */
     private function saveScreenshot(?string $screenshotUri, Card $card): Card
     {
         if (! $screenshotUri) {
@@ -50,16 +109,17 @@ class ThumbnailHelper
         }
         $cardRepository = app(CardRepository::class);
         $screenshotPath = '/'.self::IMAGE_FOLDER.'/full-screenshot/'.$card->token;
-        $screenshot = $this->getImage($screenshotUri);
-        $screenshotPathWithExtension = $screenshotPath.'.'.$screenshot->get('extension');
-        $screenshotResult = Storage::put('public'.$screenshotPathWithExtension, $screenshot->get('image')->encode($screenshot->get('extension'))->__toString());
-        if ($screenshotResult) {
-            return $cardRepository->setProperties($card, ['full_screenshot' => $screenshotPathWithExtension]);
+        $screenshot = $this->saveImage($screenshotUri, $screenshotPath);
+        if ($screenshot->get('successful')) {
+            return $cardRepository->setProperties($card, ['full_screenshot' => $screenshotPath.'.'.$screenshot->get('extension')]);
         }
 
         return $card;
     }
 
+    /**
+     * @throws Exception
+     */
     public function saveThumbnail(?string $thumbnailUri, ?string $screenshotUri, Card $card): bool
     {
         $this->saveScreenshot($screenshotUri, $card);
@@ -74,28 +134,24 @@ class ThumbnailHelper
         if ($thumbnail->isEmpty()) {
             return false;
         }
-        $imagePathWithExtension = $imagePath.'.'.$thumbnail->get('extension');
-        $fullResult = Storage::put('public'.$imagePathWithExtension, $thumbnail->get('image')->encode($thumbnail->get('extension'))->__toString());
+        $fullResult = $this->saveImage('', $imagePath, $thumbnail);
         $cardRepository = app(CardRepository::class);
 
-        if ($fullResult) {
-            $card = $cardRepository->setProperties($card, ['full_image' => $imagePathWithExtension]);
+        if ($fullResult->get('successful')) {
+            $card = $cardRepository->setProperties($card, ['full_image' => $imagePath.'.'.$fullResult->get('extension')]);
         }
 
         $thumbnailPathWithExtension = $thumbnailPath.'.'.$thumbnail->get('extension');
         $resizedImage = $this->fileHelper->makeImage($thumbnail->get('image'))->resize(251, null, static function ($constraint) {
             $constraint->aspectRatio();
         });
-        $result = Storage::put('public'.$thumbnailPathWithExtension, $resizedImage->encode($thumbnail->get('extension'))->__toString());
+        $result = $this->saveImage(null, $thumbnailPath, collect(['image' => $resizedImage, 'extension' => $thumbnail->get('extension')]));
 
-        $thumbnailUri = str_replace('storage/', '', $thumbnailUri);
-        // If we have it locally it should get deleted because we moved it to the thumbnail and image path folders
-        Storage::delete($thumbnailUri);
         if ($screenshotUri) {
             unlink($screenshotUri);
         }
 
-        if ($result) {
+        if ($result->get('successful')) {
             $card = $cardRepository->setProperties($card, [
                 'thumbnail'        => $thumbnailPathWithExtension,
                 'thumbnail_width'  => $resizedImage->width(),
